4b697ba8fd30fc0732a7ad2b6f9b0c1c4bb74415
odinokun <odinokun@gmail.com>
2020-04-11 10:33:13 +0300

    did start settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AsideComponent } from './aside/aside.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsideComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
