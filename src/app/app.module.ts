b70f37436e1b3e9b6038af8f5feb63f0cb5e0684
odinokun <odinokun@gmail.com>
2020-04-11 11:02:42 +0300

    create aside

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AsideComponent } from './aside/aside.component';
 import { MainComponent } from './main/main.component';
+import { AsidePrimaryComponent } from './aside/aside-primary/aside-primary.component';
+import { AsideSecondaryComponent } from './aside/aside-secondary/aside-secondary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AsideComponent,
-    MainComponent
+    MainComponent,
+    AsidePrimaryComponent,
+    AsideSecondaryComponent
   ],
   imports: [
     BrowserModule
